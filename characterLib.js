1d06f332434b0470dd306c6db8a87d8e6dee08be
cofcsunny <parkerst@g.cofc.edu>
2024-01-24 21:04:35 -0500

    I made it so much better, but I'm too tired to thi

--- characterLib.js
+++ characterLib.js
@@ -1,104 +1,34 @@
-//bsae
-class Prerquisite{
-    constructor(strings){
-        //
-    }
-}
-class Ability{
-    constructor(name, prerquisites){
-        //
-    }
-}
-class Handler{//an advance handler
-    currentLevel = 0
-    levelCount = 1
-    constructor(type, levels){
-        this.type = type
-        this.levels = levels
-        //levels should look like {"levelNames":["etc","etc"], "levelCosts":[200,400]}
-    }
-    getAdvanceCost(){
-        return this.levels["levelCosts"][this.currentLevel]
-    }
-    getAdvanceLevel(){
-        return this.currentLevel
-    }
-
-}
-class Skill{
-    constructor(name, costs, handler){
-        this.name = name
-        this.costs = costs
-        this.handler = handler
-    }
-    constructor(name, costs, handler, difficulty){
-        this.name = name
-        this.costs = costs
-        this.handler = handler
-        this.difficulty = difficulty
-    }
-}
-class Characteristic{
-    constructor(name, handler){
-        this.name = name
-        this.handler = handler
-    }
-}
 class Character{
     constructor(name, abilities, characteristics, skills){
         this.name = name
-        this.abilities = abilities
-        this.characteristics = characteristics
-        this.skills = skills
+        this.game_abilities = abilities
+        this.game_characteristics = characteristics
+        this.game_skills = skills
+        this.skills = {}
+        this.characteristics = {}
+        for(characteristic in this.game_characteristics){
+            this.characteristics[characteristic["name"]] = 0
+        }
+        for(skill in this.game_skills){
+            this.skills[skill["name"]] = "Untrained"
+        }
     }
 }
 
-class World{
-    constructor(){
-        //
-    }
-}
-//^world in case I need it for keeping track of objects
-//Mythic World Constants
-
-mythicCharacteristicLevels = [{"Simple":200}, {"Rookie":400}, {"Intermediate":800}, {"Trained":1200}, {"Mastery":1600}]
-mythicSkillLevels = ["Trained", "+10", "+20"]
-
-mythicCharacteristics = ["Strength", "Toughness", "Etc"]
-mythicSkills = [{"Appeal":[200,400,600],"Difficulty":"basic"}, {"Athletics":[200,400,600],"Difficulty":"basic"}]
-
-
-/////attempt at loops
-characteristicList = []
-skillsList = []
-
-for(char in mythicCharacteristics){
-    characteristicList+=Characteristic(char, Handler("Characteristic", mythicCharacteristicLevels))
-}
-
-
-
-mythicAbilities = [
-    actionFigureHands = Ability("Action Figure Hands", "Description...", Handler("Ability", 800, Prerquisite({"Strength":50})))
+mythicCharacteristics = [
+    {"name":"Strength", "levels":{"levelNames":["Simple"], "levelCosts":[200]}},
 ]
-/////
-
-
-//Mythic specific for testing
+mythicSkills = [
+    {"name":"Appeal", "levels":{"levelNames":["Trained","+10","+20"],"levelCosts":[200,400,600]},"Difficulty":"basic"},
+]
+mythicAbilities = [
+    {"name":"Action Figure Hands", "cost":800, "description":"description"},
+]
 class MythicCharacter extends Character{
     constructor(name){
-        super(name, mythicAbilities, mythicSkills)
+        super(name, mythicAbilities, mythicCharacteristics, mythicSkills)
     }
 }
 
 John = MythicCharacter("John")
 
-
-
-
-
-
-
-//give an object which contains stat information and an object with 2 arrays
-//levelNames:[]
-//levelCosts:[]
